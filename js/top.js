20cc511a7e234796c5d1303e1e49d8330e502fa9
mumu111111 <2646709035@qq.com>
2019-06-23 16:51:02 +0800

    'README'

---
+++ js/top.js
@@ -0,0 +1,18 @@
+import $ from '../vendors/jquery.min.js'
+class Top {
+    constructor(){
+        this.index = 0;
+    }
+    init (){
+        this.bind()
+        this.start()
+
+    }
+    bind(){
+
+    }
+    start(){
+        
+    }
+
+}
